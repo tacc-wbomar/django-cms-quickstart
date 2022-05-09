c7c12420cccb1dcc54157e9913f43450f5e9034a
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-05-09 18:43:45 -0500

    wip-feat(fp-1487): unique settings placeholders

---
+++ tacc_core_cms_backend/settings_custom.py
@@ -0,0 +1,2 @@
+# Do not add to this file in this repository
+# FAQ: This placeholder is overwritten by apps with this app as a dependnecy
---
+++ tacc_core_cms_backend/settings_local.py
@@ -0,0 +1,2 @@
+# Do not add to this file in this repository
+# FAQ: This placeholder can be overwritten by local instances of this app
